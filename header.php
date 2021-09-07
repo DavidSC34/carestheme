44932f6ded693f2e06f33c7b72aa75a4c52138d8
DavidSC <david@wbcboxing.com>
2021-09-07 03:39:24 -0500

    cambios menu

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
     <header class="header-cares">
         <div class="navbar-fixed">
             <nav class=" z-depth-0">
-                <a href="index.php" class="brand-logo left"><img class="header-cares--logo" src="imgs/Logo-OUT.svg" alt="Logo WBC Cares" onmouseover="this.src='imgs/Logo-OVER.svg';" onmouseout="this.src='imgs/Logo-OUT.svg';" /></a>
-                <!-- <div class="nav-wrapper" id="nav_principal"> -->
-                <!-- <ul class="right hide-on-med-and-down">
+                <div class="nav-wrapper">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" class="brand-logo left"><img class="header-cares--logo" src="<?php echo get_template_directory_uri(); ?>/imgs/Logo-OUT.svg" alt="Logo WBC Cares" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/imgs/Logo-OVER.svg';" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/imgs/Logo-OUT.svg';" /></a>
+                    <!-- <ul class="right hide-on-med-and-down">
                         <li><a class="links" href="about.php">About us</a></li>
                         <li><a class="links" href="stories.php">Stories</a></li>
                         <li><a class="links" href="ambassadors.php">Ambassadors</a></li>
@@ -25,18 +25,18 @@
                         <li><a class="links" href="contact.php">Join</a></li>
                         <li><a class="waves-effect waves-light btn" href="https://www.paypal.me/wbccares" target="_blank">Donate</a></li>
                     </ul> -->
-                <?php
+                    <?php
 
-                $args = array(
-                    'menu_class' => 'right hide-on-med-and-down',
-                    'container_id' => 'nav_principal',
-                    'container_class' => 'nav-wrapper',
-                    'theme_location' => 'menu_principal'
-                );
-                wp_nav_menu($args);
-                ?>
-                <!-- </div> -->
-                <a href="#" data-target="slide-out" class="right sidenav-trigger show-on-medium-and-down"><i class="material-icons">menu</i></a>
+                    $args = array(
+                        'menu_class' => 'right hide-on-med-and-down',
+                        'container_id' => 'nav_principal',
+
+                        'theme_location' => 'menu_principal'
+                    );
+                    wp_nav_menu($args);
+                    ?>
+                    <a href="#" data-target="slide-out" class="right sidenav-trigger show-on-medium-and-down"><i class="material-icons">menu</i></a>
+                </div>
             </nav>
         </div>
         <ul id="slide-out" class="sidenav">
